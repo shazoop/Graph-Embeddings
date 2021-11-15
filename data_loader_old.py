fixed calculation errors. implemented filtering by connectiity

data_loader_old.py
@@ -1,8 +1,7 @@
 import numpy as np
-import os
 import math
 
-def get_sets(path, edge_filter = False, max_edges = None):
+def get_sets(path):
     """
     Parses the raw FB text files and returns the set of all entities(their machine ids) and relations
     
@@ -27,32 +26,10 @@ def get_sets(path, edge_filter = False, max_edges = None):
     
     ent_id = {k:i for (i,k) in enumerate(sorted(entities))}
     rel_id = {k:i for (i,k) in enumerate(sorted(relations))}
-    
-    if edge_filter is True:
-        assert max_edges != None, 'need to specify maximum number of edges when filtering'
-        connect_count = {k:0 for k in list(ent_id.keys())}
-        with open(os.path.join(path, 'train.txt'), "r") as lines:
-            for line in lines:
-                lhs, rel, rhs = line.strip().split("\t")
-                connect_count[lhs] = connect_count[lhs] + 1
-                connect_count[rhs] = connect_count[rhs] + 1
-
-        keep_ent = [k for k,v in connect_count.items() if v <= max_edges]
-        ent_id = {k:i for i,k in enumerate(keep_ent)}
-        edge_set = {}
-        for split in ["train.txt", "valid.txt", "test.txt"]:
-            with open(os.path.join(path, split), "r") as lines:
-                edges = set()
-                for line in lines:
-                    lhs, rel, rhs = line.strip().split("\t")
-                    if (lhs in keep_ent) and (rhs in keep_ent):
-                        edges.add((lhs,rel,rhs))
-            edge_set[split] = edges
-
-               
+                
     return ent_id, rel_id, edge_set
 
-def get_codebook(path, D_ent, D_rel, edge_filter = False, max_edges = None):
+def get_codebook(path, D_ent, D_rel):
     """
     Generates a codebook for the set of entities and relations by randomly sampling from the unit hyperspheres of dimension
     D_ent and D_rel respectively. 
@@ -61,7 +38,7 @@ def get_codebook(path, D_ent, D_rel, edge_filter = False, max_edges = None):
     
     output: dictionary of embeddings for entity and relation set (machine ids are keys for entity), plus output from get_id
     """
-    ent_id, rel_id, edge_set = get_sets(path, edge_filter, max_edges)
+    ent_id, rel_id, edge_set = get_sets(path)
     
     def normal_vec(dim):
         vec = np.random.multivariate_normal(np.zeros(dim),np.eye(dim))
@@ -72,48 +49,38 @@ def get_codebook(path, D_ent, D_rel, edge_filter = False, max_edges = None):
         return np.append(1.,vec/np.linalg.norm(vec))
 
     
-    ent_code = {k:normal_vec(D_ent) for k in list(ent_id.keys())}
-    rel_code = {k:nv_append1(D_rel) for k in list(rel_id.keys())}
+    ent_code = {k:normal_vec(D_ent) for (i,k) in enumerate(list(ent_id.keys()))}
+    rel_code = {k:nv_append1(D_rel) for (i,k) in enumerate(list(rel_id.keys()))}
     
     return ent_id, rel_id, ent_code, rel_code, edge_set
 
-def get_embeddings(split, ent_id, rel_id, ent_code, rel_code, edge_set, rel_tensor = False):
+def get_embeddings(path, split, D_ent, D_rel, rel_tensor = False):
     """
     For each entity, generate the embedding of its neighborhood subgraph (all edges involving the entity).
     
     input: path object, data split, entity embedding dim, relation embedding dim
     """
+    ent_id, rel_id, ent_code, rel_code, edge_set = get_codebook(path, D_ent, D_rel)
     N_ent = len(ent_id)
-    D_ent = (list(ent_code.values())[0]).shape[0]
-    D_rel = (list(rel_code.values())[0]).shape[0]
     
     if rel_tensor == True:
         embeddings = np.zeros((N_ent,D_ent,D_ent,D_rel+1))
     else:
         embeddings = np.zeros((N_ent,D_ent,D_ent))
     
-    for e in list(edge_set[split]):
-        head, rel, tail = e[0], e[1], e[2]
-        head_ebd, rel_ebd, tail_ebd = ent_code[head], rel_code[rel], ent_code[tail]
-        if rel_tensor == True:
-            tensor = np.einsum('i,j,k -> ijk',head_ebd,tail_ebd,rel_ebd)
-        else:
-            tensor = np.einsum('i,j -> ij', head_ebd,tail_ebd)
-        embeddings[ent_id[head]] += tensor
-        embeddings[ent_id[tail]] += tensor
+    with open(os.path.join(path, split), "r") as lines:
+        for line in lines:
+            lhs, rel, rhs = line.strip().split("\t")
+            head = ent_code[lhs]
+            tail = ent_code[rhs]
+            relation = rel_code[rel]
+            if rel_tensor == True:
+                tensor = np.einsum('i,j,k -> ijk',head,tail,relation)
+            else:
+                tensor = np.einsum('i,j -> ij', head, tail)
+            embeddings[ent_id[lhs]] += tensor
+            embeddings[ent_id[rhs]] += tensor
                 
-    return embeddings
-
-def easy_embeddings(path, split, D_ent, D_rel, rel_tensor = False, edge_filter = False, max_edges = None):
-    """
-    For each entity, generate the embedding of its neighborhood subgraph (all edges involving the entity).
-    
-    input: path object, data split, entity embedding dim, relation embedding dim
-    """
-    ent_id, rel_id, ent_code, rel_code, edge_set = get_codebook(path, D_ent, D_rel, edge_filter, max_edges)
-    
-    embeddings =   get_embeddings(split, ent_id, rel_id, ent_code, rel_code, edge_set, rel_tensor)
-    
     return embeddings, ent_id, rel_id, ent_code, rel_code, edge_set
 
 def avg_ePn(edge_set):

embedding_torch.py
@@ -1,16 +1,14 @@
 import numpy as np
 import torch
+import random
 
-def embed_to_device(ent_code,rel_code, ent_embeddings, rel_embeddings, device):
-    '''
-    Given the dictionaries created by "get_embedding", will load them onto the appropriate device
-    '''
+def embed_to_device(nbd_embeddings, ent_code, rel_code, device):
     ent_embeddings = torch.from_numpy(np.stack(list(ent_code.values()),0))
     rel_embeddings = torch.from_numpy(np.stack(list(rel_code.values()),0))
     ent_embeddings = ent_embeddings.to(device)
     rel_embeddings = rel_embeddings.to(device)
-    nbd_embeddings = torch.from_numpy(embeddings).to(device) 
-    return ent_embeddings, rel_embeddings, ent_embeddings, rel_embeddings, ent_embeddings
+    nbd_embeddings = torch.from_numpy(nbd_embeddings).to(device) 
+    return nbd_embeddings, ent_embeddings, rel_embeddings
 
 def tch_projmatrix(ent, ent_embeddings):
     '''
@@ -43,25 +41,42 @@ def tch_score(edge,ent_embeddings, rel_embeddings, nbd_embeddings, ent_id, rel_i
     
     #Generate the projection matrices for the given head
     proj = tch_projmatrix(head_embed, ent_embeddings)
-    Frob_norm = torch.norm(head_nbd)**2
+    Frob_norm_sq = (torch.norm(head_nbd)**2).cpu().item()
     #Generate the 
     
     #Compute the graph homomorphism coeff
-    x = torch.einsum('nij,jk -> nik',proj,head_nbd) #left multiply by projection matrix
-    x = torch.einsum('nij,nkj -> nik',x,proj) #right multiply by transpose
+    x1 = torch.einsum('nij,jk -> nik',proj,head_nbd) #left multiply by projection matrix
+    x2 = torch.einsum('ij,nkj -> nik',head_nbd,proj) #right multiply by transpose projection
+    x = x1 + x2
     res = torch.bmm(x.permute(0,2,1),nbd_embed_norel)
-    coeff = (1/Frob_norm)*torch.einsum('nii -> n',res) #number of matching edges
+    coeff = (1/(Frob_norm_sq + 1e-3))*torch.einsum('nii -> n',res) #number of matching edges
     
     #Get the top k
     (val,ix) = torch.topk(coeff,k)
-    edge_coeff = torch.zeros(k-1).to(device)
+    edge_coeff = torch.zeros(k-1)
     for i in range(1,k): #ignore top match, since that's likely to be nbd_embedding of head itself
         curr_ix = ix[i].cpu().item()
-        edge_coeff[i-1] = torch.einsum('i,ij,j->',head_embed,nbd_embeddings[curr_ix],tail_embed).cpu().item()
+        edge_coeff[i-1] = torch.einsum('i,ij,j->',ent_embeddings[curr_ix],nbd_embeddings[curr_ix],tail_embed).cpu().item()
+#         edge_coeff = 2*edge_coeff.clamp(min=0.,max=1.) - 1
     
     #Compute score by weighting each score (-1,1) by softmax of the similarities
-    score = torch.dot(torch.nn.functional.softmax(val[1:],dim=0).float(),edge_coeff)
+    score = torch.dot(val[1:].float().cpu(),edge_coeff.cpu())
+
     
-    return score
+    return score, val, edge_coeff, Frob_norm_sq
     
+def test(num_batch, ent_embeddings, rel_embeddings, nbd_embeddings, ent_id, rel_id, k, edge_set, rel_tensor= False):
+    edges = list(edge_set['test.txt'])
+    batch = random.sample(edges,num_batch)
+    ttl_score = 0
+    num_edges = 0
+    
+    for e in batch:
+        score, _ , _, Frob_norm_sq = tch_score(e,ent_embeddings, rel_embeddings, nbd_embeddings, ent_id, rel_id, k, rel_tensor)
+        if Frob_norm_sq <= 1e-3:
+            continue
+        ttl_score = ttl_score + score.cpu().item()
+        num_edges = num_edges + 1
+    
+    return ttl_score/num_edges
     